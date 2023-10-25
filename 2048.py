c636408803e781d73e72123dc58dcffc29df102a
Gleb Avershin <glebus_avershin@mail.ru>
2023-10-25 18:01:54 +0300

    Add set up

--- 2048.py
+++ 2048.py
@@ -1,2 +1,13 @@
+import pygame
+import random
+pygame.init()
 
+# initial set up
+WIDTH = 400
+HEIGHT = 500
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('2048')
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 24)
 
